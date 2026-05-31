439d1d41ad81d643c5c340c9ddd8f3765b738921
JAY-WIR <2425400681@live.gctu.edu.gh>
2026-05-31 08:24:52 +0000

    THIS IS MY ASSIGNMENT

--- initials.py
+++ initials.py
@@ -1,6 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: ANN-JULIA WIREKO
+# STUDENT ID: 2425400681
+# Email: 2425400681@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, ANN-JULIA WIREKO, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large stylized letters.
+
+    print()
+    print("...AJW")
+    print()
+
+    print("...AAAAAAAAAAAA****...JJJJJJJJJJJJ****...WW........WW****")
+    print("...AAAA....AAAA****...JJJJJJJJJJJJ****...WW........WW****")
+    print("...AAAA....AAAA****........JJJJ....****...WW........WW****")
+    print("...AAAA....AAAA****........JJJJ....****...WW...WW...WW****")
+    print("...AAAAAAAAAAAA****........JJJJ....****...WW...WW...WW****")
+    print("...AAAAAAAAAAAA****........JJJJ....****...WW...WW...WW****")
+    print("...AAAA....AAAA****........JJJJ....****...WW..WWWW..WW****")
+    print("...AAAA....AAAA****...JJJJ.JJJJ....****...WW.WW..WW.WW****")
+    print("...AAAA....AAAA****...JJJJJJJJJ....****...WWWW....WWWW****")
+    print("...AAAA....AAAA****....JJJJJJJ.....****...WWW......WWW****")
+
+    print()
 
 
 
